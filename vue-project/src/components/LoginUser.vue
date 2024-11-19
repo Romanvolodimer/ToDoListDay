3a95706845023339216b23958a20b430002cd4e6
Romanvolodimer <romanvolodimer@gmail.com>
2024-11-19 13:06:54 +0200

    add my dotolist

---
+++ vue-project/src/components/LoginUser.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { InputText } from "primevue";
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import FloatLabel from "primevue/floatlabel";
+import Button from "primevue/button";
+
+import { computed, ref } from "vue";
+
+const username = ref<string>("");
+const password = ref<string>("");
+const isLoginDisabled = computed(() => !username.value || !password.value);
+</script>
+<template>
+  <div class="login flex flex-column row-gap-5">
+    <InputGroup>
+      <InputGroupAddon>
+        <i class="pi pi-user"></i>
+      </InputGroupAddon>
+      <FloatLabel>
+        <InputText id="username" v-model="username" />
+        <label for="username">Username</label>
+      </FloatLabel>
+    </InputGroup>
+    <InputGroup>
+      <InputGroupAddon>
+        <i class="pi pi-lock"></i>
+      </InputGroupAddon>
+      <FloatLabel>
+        <InputText type="password" id="password" v-model="password" />
+        <label for="password">Password</label>
+      </FloatLabel>
+    </InputGroup>
+
+    <Button label="Submit" :disabled="isLoginDisabled" />
+  </div>
+</template>
+<style>
+.login {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import LoginUser from "../components/LoginUser.vue";
+</script>
+
+<template>
+  <div class="login">
+    <h1 class="head green">Log in your ToDolist</h1>
+    <LoginUser />
+  </div>
+</template>
+
+<style>
+h1 {
+  padding-bottom: 30px;
+}
+</style>
